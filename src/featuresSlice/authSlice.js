d91e286043a84a89d3916a5014f73deb44134648
Paras <mehtaparas1901@gmail.com>
2024-01-19 18:40:46 +0530

    configure redux toolkit

---
+++ src/featuresSlice/authSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const intialState = {
+    status: false,  //initially user is not authenticated
+    userData: null
+}
+
+
+//this is used to track user (authenticated or not)
+const authSlice = createSlice({
+      name: "auth",
+      intialState,
+
+      reducers: {
+         login : (state, action) => {
+            state.status = true
+            state.userData = action.payload.userData
+         },
+
+         logout : (state, action) => {
+            state.status = false
+            state.userData = false
+         }
+      }
+   }
+)
+
+//exporting all reducers
+export default authSlice.reducer
+
+//exporting all actions(methods inside reducer) separately as they will be used in different components
+export const {login, logout} = authSlice.actions
