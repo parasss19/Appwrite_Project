e37d83d63c768488c314da9e9c5f13de553d5fb5
Paras <mehtaparas1901@gmail.com>
2024-01-20 15:17:26 +0530

    conflicts fix

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,9 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 
-export {Header, Footer, Container}
+export {  
+    Header,
+    Footer, 
+    Container,
+    
+}
